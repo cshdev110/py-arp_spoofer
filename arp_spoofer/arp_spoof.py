a8dfc9c7b9e00ce5d2ff8aadb421dcc64914b8b9
hostuser <hostuser@hostuser.hostuser>
2022-07-06 23:53:41 +1000

    ARP Spoof v1.2 With broad exception, BaseException, to warn about it and reset the ipv4.ip_forward to 0.

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -67,9 +67,8 @@ except KeyboardInterrupt:
     if not ip_forward_flag:
         ipv4 = subprocess.run(["sysctl", "-w", "net.ipv4.ip_forward=0"], capture_output=True)
         print(ipv4.stdout.decode().replace("net.ipv4.", "Returning ipv4 "))
-except Exception:
-    restore(target_ip, target_MAC, gateway_ip, spoof_mac)
-    restore(gateway_ip, spoof_mac, target_ip, target_MAC)
+except BaseException as be:
+    print("\r[-] " + be.__str__() + " error has come up ... quiting", end='\n\n')
     if not ip_forward_flag:
         ipv4 = subprocess.run(["sysctl", "-w", "net.ipv4.ip_forward=0"], capture_output=True)
         print(ipv4.stdout.decode().replace("net.ipv4.", "Returning ipv4 "))
